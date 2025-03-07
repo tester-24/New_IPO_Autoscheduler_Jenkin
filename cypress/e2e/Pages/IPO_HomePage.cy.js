a14cc3fc5c9d79d6724c9718a74c0b4592daaf18
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-03-07 17:37:34 +0530

    IPO_REPORTS.

--- cypress/e2e/Pages/IPO_HomePage.cy.js
+++ cypress/e2e/Pages/IPO_HomePage.cy.js
@@ -137,97 +137,97 @@ it('Ipo _Jainam', () => {
   //  cy.get('.init-close').click()
 
   
-     // Assertion for OPEN IPO 
-     cy.wait(1000)
-    cy.xpath('/html/body/app-root/app-layout/app-maincontent/app-clientdashboard/section[2]/div/div/div[1]/div[2]/div[1]').should('be.visible')
+   //   // Assertion for OPEN IPO 
+   //   cy.wait(1000)
+   //  cy.xpath('/html/body/app-root/app-layout/app-maincontent/app-clientdashboard/section[2]/div/div/div[1]/div[2]/div[1]').should('be.visible')
      
 
-   //First test Open IPO contains date  if data is present click on Apply button
-   cy.wait(500)
-     if(cy.xpath('/html/body/app-root/app-layout/app-maincontent/app-clientdashboard/section[2]/div/div/div[1]/div[2]/div[2]/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody').should('have.length.above',0))
-     {
-        cy.wait(3000)        
-        if (cy.get('.btn-grid-bid > .btn').should('be.visible')) 
-                  {
-                    //Click on apply button 
-                    cy.get('[data-kendo-grid-item-index="0"] > [data-kendo-grid-column-index="5"] > .btn-grid-bid > .btn').click({force:true},{timeout:5000},{multiple:true})
-                    cy.wait(1000)
-                    //click on ok button for new dailog box
-                 //   cy.get('.bd-example-modal-md > .modal-dialog > .modal-content > .modal-footer > .btn').click()
-                    cy.wait(1000)
-                    cy.visit('https://ipo.jainam.in');
-            }
-      }
+   // //First test Open IPO contains date  if data is present click on Apply button
+   // cy.wait(500)
+   //   if(cy.xpath('/html/body/app-root/app-layout/app-maincontent/app-clientdashboard/section[2]/div/div/div[1]/div[2]/div[2]/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody').should('have.length.above',0))
+   //   {
+   //      cy.wait(3000)        
+   //      if (cy.get('.btn-grid-bid > .btn').should('be.visible')) 
+   //                {
+   //                  //Click on apply button 
+   //                  cy.get('[data-kendo-grid-item-index="0"] > [data-kendo-grid-column-index="5"] > .btn-grid-bid > .btn').click({force:true},{timeout:5000},{multiple:true})
+   //                  cy.wait(1000)
+   //                  //click on ok button for new dailog box
+   //               //   cy.get('.bd-example-modal-md > .modal-dialog > .modal-content > .modal-footer > .btn').click()
+   //                  cy.wait(1000)
+   //                  cy.visit('https://ipo.jainam.in');
+   //          }
+   //    }
     
-     //Check if Open IPO contains data 
+   //   //Check if Open IPO contains data 
     
-     //table[class="k-grid-table k-table k-table-md"]>tbody>tr
-     cy.wait(3000)
+   //   //table[class="k-grid-table k-table k-table-md"]>tbody>tr
+   //   cy.wait(3000)
 
-    if(cy.get('table[class="k-grid-table k-table k-table-md"] >tbody> tr:nth-child(2)').should('have.length.above',0))
-      {
-           cy.xpath('/html/body/app-root/app-layout/app-maincontent/app-clientdashboard/section[2]/div/div/div[1]/div[1]/div').click({force:true})
-         //  cy.get('.initial-social-icons > :nth-child(1) > .fa').should('be.visible')
-         //  cy.wait(2000)
-         //  cy.get(':nth-child(2) > .fa').should('be.visible')
-          // cy.wait(1000)
-           cy.get('.init-close').click({force:true})
-      }
+   //  if(cy.get('table[class="k-grid-table k-table k-table-md"] >tbody> tr:nth-child(2)').should('have.length.above',0))
+   //    {
+   //         cy.xpath('/html/body/app-root/app-layout/app-maincontent/app-clientdashboard/section[2]/div/div/div[1]/div[1]/div').click({force:true})
+   //       //  cy.get('.initial-social-icons > :nth-child(1) > .fa').should('be.visible')
+   //       //  cy.wait(2000)
+   //       //  cy.get(':nth-child(2) > .fa').should('be.visible')
+   //        // cy.wait(1000)
+   //         cy.get('.init-close').click({force:true})
+   //    }
 
-       // Scrolling
-     //cy.wait(2000)
-     cy.scrollTo(0,1000)
+   //     // Scrolling
+   //   //cy.wait(2000)
+   //   cy.scrollTo(0,1000)
   
           
-      //Assertion for  Upcoming IPO
-      //cy.wait(4000)
-      cy.xpath('//*[@id="accordion"]/div[1]/div/h2').should('be.visible')
+   //    //Assertion for  Upcoming IPO
+   //    //cy.wait(4000)
+   //    cy.xpath('//*[@id="accordion"]/div[1]/div/h2').should('be.visible')
 
-     //Check if Upcoming IPO contains data 
-      if(cy.get('#collapseOne').should('have.length.above',0))
-        {
-          cy.get('[href="#collapseTwo"]').click({force:true})
-        }
+   //   //Check if Upcoming IPO contains data 
+   //    if(cy.get('#collapseOne').should('have.length.above',0))
+   //      {
+   //        cy.get('[href="#collapseTwo"]').click({force:true})
+   //      }
       
-         // Check SEBI contains data
-         if(cy.get('#collapseTwo > .k-grid').should('have.length.above',0))
-          {
-                     cy.log("SEBI grid Contains data ")
-         }  
-        // cy.wait(3000) 
-         cy.scrollTo('center')          
+   //       // Check SEBI contains data
+   //       if(cy.get('#collapseTwo > .k-grid').should('have.length.above',0))
+   //        {
+   //                   cy.log("SEBI grid Contains data ")
+   //       }  
+   //      // cy.wait(3000) 
+   //       cy.scrollTo('center')          
         
-        //  //Test on 2nd page ,data is present   ----ON SEBI GRID 
-        // cy.get('#collapseTwo > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 2"]').click()
-        // cy.wait(2000)
-        // if(cy.get('#collapseTwo > .k-grid').should('have.length.above',0))
-        // {
-        //   cy.log('Second page contains data ')
+   //      //  //Test on 2nd page ,data is present   ----ON SEBI GRID 
+   //      // cy.get('#collapseTwo > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 2"]').click()
+   //      // cy.wait(2000)
+   //      // if(cy.get('#collapseTwo > .k-grid').should('have.length.above',0))
+   //      // {
+   //      //   cy.log('Second page contains data ')
           
-        // }
+   //      // }
         
-        // //Test on 4TH page ,data is present   ----ON SEBI GRID 
-        // cy.scrollTo('center')
-        // cy.get('#collapseTwo > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 4"]').click()
-        // //cy.wait(2000)
-        // if(cy.get('#collapseTwo > .k-grid').should('have.length.above',0))
-        // {
-        //   cy.log('Fourth page contains data ')
+   //      // //Test on 4TH page ,data is present   ----ON SEBI GRID 
+   //      // cy.scrollTo('center')
+   //      // cy.get('#collapseTwo > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 4"]').click()
+   //      // //cy.wait(2000)
+   //      // if(cy.get('#collapseTwo > .k-grid').should('have.length.above',0))
+   //      // {
+   //      //   cy.log('Fourth page contains data ')
 
-        // }
+   //      // }
 
-        // Assertion for Close IPO
-        //cy.wait(3000)
-        cy.get('.custom_close_gridtable > .ipo-left-grid').should('have.length.greaterThan',0)
-        // click on 3rd page 
-        //cy.wait(2000)
-        cy.get('.custom_close_gridtable > .ipo-left-grid > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 3"]').click({force:true})
-        //Click on 5th Page 
-        //cy.wait(1000)
-        cy.get('.custom_close_gridtable > .ipo-left-grid > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 5"]').click({force:true})
-        cy.get('.footer-logo > img').should('be.visible')
-        cy.get('#return-to-top').click()
-                    //Click on Apply for ipo
+   //      // Assertion for Close IPO
+   //      //cy.wait(3000)
+   //      cy.get('.custom_close_gridtable > .ipo-left-grid').should('have.length.greaterThan',0)
+   //      // click on 3rd page 
+   //      //cy.wait(2000)
+   //      cy.get('.custom_close_gridtable > .ipo-left-grid > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 3"]').click({force:true})
+   //      //Click on 5th Page 
+   //      //cy.wait(1000)
+   //      cy.get('.custom_close_gridtable > .ipo-left-grid > .k-grid > .k-pager > .k-pager-numbers-wrap > .k-pager-numbers > [aria-label="Page 5"]').click({force:true})
+   //      cy.get('.footer-logo > img').should('be.visible')
+   //      cy.get('#return-to-top').click()
+   //                  //Click on Apply for ipo
   
             // logout flow
             //click on profile icon 
