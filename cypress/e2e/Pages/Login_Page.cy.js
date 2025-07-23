f486d6c3020cca29fe397c22337ff3002af298d6
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-07-23 18:53:28 +0530

    Update Login_Page.cy.js

--- cypress/e2e/Pages/Login_Page.cy.js
+++ cypress/e2e/Pages/Login_Page.cy.js
@@ -7,12 +7,12 @@ export class Login_Page{
     enterUsername1() 
     {
         //cy.get(':nth-child(2) > .text_form').clear()
-        cy.get('.userid_wrap > .input_wrap > .text_form').type('1512')
+        cy.get('.userid_wrap > .input_wrap > .text_form').type('J33')
     }
 
     enterPassword1() 
     {
-        cy.get('#LoginPassword').type('Jainam@123')
+        cy.get('#LoginPassword').type('Alpha@123')
     }
 
     clickLogin1() 
