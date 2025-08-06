bf10af42b5db56e90ce6a5839db6079beb72ce52
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-08-06 16:06:25 +0530

    Bulk_Printing_IPO.cy.js

--- cypress/e2e/Bulk_Printing_IPO.cy.js
+++ cypress/e2e/Bulk_Printing_IPO.cy.js
@@ -24,10 +24,10 @@ cy.wait(5000);
   cy.wait(3000);
 
   // Enter OTP
-  cy.xpath("//input[@id='pin1']").type("1");
-  cy.xpath("//input[@id='pin2']").type("2");
-  cy.xpath("//input[@id='pin3']").type("3");
-  cy.xpath("//input[@id='pin4']").type("4");
+  cy.xpath("//input[@id='pin1']").type("8");
+  cy.xpath("//input[@id='pin2']").type("5");
+  cy.xpath("//input[@id='pin3']").type("2");
+  cy.xpath("//input[@id='pin4']").type("0");
   cy.wait(5000);
       cy.xpath("//button[@aria-label='Close']").click({force:true});
      
@@ -119,3 +119,4 @@ cy.wait(5000);
      })
    })
   })  
+
